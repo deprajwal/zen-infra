0739d277d64213b982fbe01b194f6c7ac27afeec
deprajwal <dekateprajwal007@gmail.com>
2026-05-18 17:31:30 +0530

    Change availability zones for public subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[count.index]
-  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
